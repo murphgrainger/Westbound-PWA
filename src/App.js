e680b544badb0876bcff0c0911f166dae445dc0e
Murph Grainger <murph.grainger@gmail.com>
2020-03-03 15:06:44 -0700

    added sass and reorganized files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './westbound-logo.png';
-import './App.css';
+import logo from './assets/logos/westbound-logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Westbound Place and Co logo" />
         <p>Nothing ventured, nothing gained.</p>
       </header>
     </div>
