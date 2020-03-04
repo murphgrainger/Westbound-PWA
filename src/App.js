6f4465e51c681d9d0f24862f84c2bb44c6de0727
Murph Grainger <murph.grainger@gmail.com>
2020-03-04 14:42:10 -0700

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
+import Navbar from './components/Navbar';
 import logo from './assets/logos/westbound-logo.png';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="Westbound Place and Co logo" />
         <p>Nothing ventured, nothing gained.</p>
